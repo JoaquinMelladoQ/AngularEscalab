c639b55bc610a6befa9d9e9186d9af1f572dcfda
Joaquín Mellado Quevedo <mellado.joaquin@gmail.com>
2021-11-07 18:19:07 -0300

    Initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageListComponent } from './Views/image-list/image-list.component';
+import { AuthorListComponent } from './Views/author-list/author-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageListComponent,
+    AuthorListComponent
   ],
   imports: [
     BrowserModule,
